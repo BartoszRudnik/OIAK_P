04d1851eef185944c29a6ac0ab10c0cc2e1d8768
Bartosz Rudnik <rudnik49@gmail.com>
2020-05-25 22:31:11 +0200

    first commit

---
+++ QuickSort.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+using namespace std;
+
+int Partition(int tab[], int p, int r){
+
+    int x = tab[r];
+    int i = p - 1;
+
+    for(int j = p; j < r; j++){
+
+        if(tab[j] <= x){
+
+            i++;
+            swap(tab[i],tab[j]);
+
+        }
+
+    }
+
+    swap(tab[i + 1], tab[r]);
+    return (i + 1);
+
+}
+
+void QuickSort(int tab[], int p, int r){
+
+    if(p < r){
+
+        int q = Partition(tab, p, r);
+
+        QuickSort(tab, p, q - 1);
+        QuickSort(tab, q + 1, r);
+
+    }
+
+}
---
+++ cmake-build-debug/CMakeFiles/OIAK_P.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OIAK_P.dir/QuickSort.cpp.o"
+  "CMakeFiles/OIAK_P.dir/main.cpp.o"
+  "OIAK_P"
+  "OIAK_P.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OIAK_P.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
