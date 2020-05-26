8bb08e07c1a586f74e5fa613a213626cfb2bd0d8
Bartosz Rudnik <rudnik49@gmail.com>
2020-05-26 18:11:44 +0200

    wstepne menu

--- main.cpp
+++ main.cpp
@@ -4,14 +4,43 @@ using namespace std;
 
 int main() {
 
-    int n;
+    int n, nrAlg;
+    bool spr = true;
 
     cout << "Podaj liczbe danych do posortowania: ";
     cin >> n;
 
-    testSingleThread(n);
-    testNoLimit(n);
-    testLimit(n);
+    while(spr){
+
+        cout << "Wybierz wersje algorytmu: " << endl;
+        cout << "1. Wersja jednowatkowa" << endl;
+        cout << "2. Wersja wielowatkowa (bez ograniczen liczby watkow)" << endl;
+        cout << "3. Wersja wielowatkowa (mozliwosc wyboru maksymalnej liczby watkow)" << endl;
+        cout << "0. Wyjscie" << endl;
+
+        cin >> nrAlg;
+
+        switch(nrAlg){
+
+            case 0:
+                spr = false;
+                break;
+            case 1:
+                testSingleThread(n);
+                break;
+            case 2:
+                testNoLimit(n);
+                break;
+            case 3:
+                testLimit(n);
+                break;
+            default:
+                cout << "Wybrano zly numer" << endl;
+                break;
+
+        }
+
+    }
 
     return 0;
 
